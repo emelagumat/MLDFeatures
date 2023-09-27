8030f6c2d25816ca91cfe18524e1e7e5fbdae36d
Manu <emelagumat@gmail.com>
2023-09-27 13:19:18 +0200

    Added more tests

--- Tests/MLDFeaturesTests/RemoteImage/RemoteImageFeatureTests.swift
+++ Tests/MLDFeaturesTests/RemoteImage/RemoteImageFeatureTests.swift
@@ -10,11 +10,16 @@ class RemoteImageFeatureTests: XCTestCase {
     var store: TestStoreOf<RemoteImageFeature>!
     var reducer: RemoteImageFeature!
     var state: RemoteImageFeature.State!
+    var sampleImage: UIImage!
+    let sampleURL = "sample"
     
     override func setUpWithError() throws {
         reducer = .init()
         state = .init()
-        store = .init(initialState: state, reducer: { reducer })
+        sampleImage = .init()
+        store = .init(initialState: state, reducer: { reducer }, withDependencies: {
+            $0.environment.loader = self
+        })
     }
     
     func testOnAppear() async {
@@ -22,20 +27,51 @@ class RemoteImageFeatureTests: XCTestCase {
     }
     
     func testOnGetStringURL() async {
-        let sampleURL = "sample"
-        
-        await store.send(.onGetStringURL(sampleURL)) {
+        await store.send(.onGetStringURL(sampleURL)) { [unowned self] in
             $0.imageStringURL = sampleURL
             $0.isLoading = true
             $0.redactedReasons = [.placeholder]
         }
+        
+        await store.receive(.onLoaded(.init()), timeout: .milliseconds(10)) { [unowned self] in
+            $0.image = .init(uiImage: sampleImage)
+            $0.isLoading = false
+            $0.redactedReasons = []
+        }
+        
+        await store.send(.onGetStringURL(sampleURL))
     }
     
     func testOnGetImage() async {
         let image = UIImage(systemName: "circle.fill")
-        
         await store.send(.onLoaded(image)) {
             $0.image = .init(uiImage: image!)
         }
+    }
+    
+    func testWithoutRedactedReasons() async {
+        store = .init(
+            initialState: state,
+            reducer: { reducer },
+            withDependencies: {
+                $0.environment.redactionEnabled = false
+            }
+        )
+        
+        await store.send(.onGetStringURL(sampleURL)) { [unowned self] in
+            $0.imageStringURL = sampleURL
+            $0.isLoading = true
+            $0.redactedReasons = []
+        }
+    }
+}
+
+extension RemoteImageFeatureTests: ImageProvider {
+    func loadImage(stringURL: String, storeInCache: Bool) async -> UIImage? {
+        sampleImage
+    }
+    
+    func loadImage(url: URL, storeInCache: Bool) async -> UIImage? {
+        sampleImage
     }
 }
